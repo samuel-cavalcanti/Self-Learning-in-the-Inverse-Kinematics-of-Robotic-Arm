b8e9ff3adbb67e5bfec2d69bd86310587b57478b
samuel-cavalcanti <scavalcanti111@gmail.com>
2017-11-09 17:16:33 -0300

    add pasta build

---
+++ build/CMakeFiles/cppremoteapi.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/remoteApi/extApi.c" "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/build/CMakeFiles/cppremoteapi.dir/remoteApi/extApi.c.o"
+  "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/remoteApi/extApiPlatform.c" "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/build/CMakeFiles/cppremoteapi.dir/remoteApi/extApiPlatform.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "MAX_EXT_API_CONNECTIONS=255"
+  "NON_MATLAB_PARSING"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../remoteApi"
+  "../Vdc"
+  "../SOM"
+  "../AuraArmVrep"
+  "../Extras"
+  "/usr/include/opencv"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/AuraArmVrep/AuraArmVrep.cpp" "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/build/CMakeFiles/cppremoteapi.dir/AuraArmVrep/AuraArmVrep.cpp.o"
+  "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/Extras/extras.cpp" "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/build/CMakeFiles/cppremoteapi.dir/Extras/extras.cpp.o"
+  "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/SOM/DataSet.cpp" "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/build/CMakeFiles/cppremoteapi.dir/SOM/DataSet.cpp.o"
+  "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/SOM/Node.cpp" "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/build/CMakeFiles/cppremoteapi.dir/SOM/Node.cpp.o"
+  "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/SOM/SOM.cpp" "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/build/CMakeFiles/cppremoteapi.dir/SOM/SOM.cpp.o"
+  "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/SOM/Sample.cpp" "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/build/CMakeFiles/cppremoteapi.dir/SOM/Sample.cpp.o"
+  "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/SOM/Test.cpp" "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/build/CMakeFiles/cppremoteapi.dir/SOM/Test.cpp.o"
+  "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/Vdc/Vdc.cpp" "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/build/CMakeFiles/cppremoteapi.dir/Vdc/Vdc.cpp.o"
+  "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/main.cpp" "/home/samuel/Documentos/Repositories/Self-Learning-in-the-Inverse-Kinematics-of-Robotic-Arm/build/CMakeFiles/cppremoteapi.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "MAX_EXT_API_CONNECTIONS=255"
+  "NON_MATLAB_PARSING"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../remoteApi"
+  "../Vdc"
+  "../SOM"
+  "../AuraArmVrep"
+  "../Extras"
+  "/usr/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
